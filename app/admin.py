7d6720a45e0b9f8d5257db810e5f7a6ba41971e8
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-03-31 20:02:09 +0700

    added admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Prediction, Integration, Sensor
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Prediction)
+admin.site.register(Integration)
+admin.site.register(Sensor)
