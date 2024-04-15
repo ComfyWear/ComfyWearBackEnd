57a907231ce15210dee95ff2816d0763752715a0
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-04-15 15:39:12 +0700

    Clean PredictViewSet

--- app/views/PredictViewSet.py
+++ app/views/PredictViewSet.py
@@ -1,10 +1,13 @@
 import os
+
 import cv2
+import numpy as np
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from rest_framework import status, viewsets
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from app.models import Integration, Prediction, Image, Sensor
@@ -22,52 +25,106 @@ class PredictViewSet(viewsets.ViewSet):
         Create a new Prediction object for a specific integration.
 
         :param request: The HTTP request with prediction data.
+        :type request: rest_framework.request.Request
         :return: Response with created prediction or error.
+        :rtype: rest_framework.response.Response
         """
         secret = request.data.get('secret')
         image_file = request.data.get('image')
 
         if secret and image_file:
-            integration = Integration.objects.filter(secret=secret).first()
+            integration = self._get_or_create_integration(secret)
+            image_path = self._save_image(image_file)
+            annotated_image, labels = self._segment_image(image_path)
+            self._save_predictions(labels, integration)
+            self._save_annotated_image(annotated_image, request, integration)
+            response_data = self._get_response_data(integration, request)
 
-            if not integration:
-                integration = Integration.objects.create(secret=secret)
-
-            image_path = self.save_image(image_file)
-            segmentation = ImageSegmentation()
-            annotated_image, labels = segmentation.segment_image(image_path)
-            self.save_predictions(labels, integration)
-            self.save_annotated_image(annotated_image, request, integration)
-            response_data = self.get_response_data(integration, request)
-
-            local_temp, local_humid = self.get_sensor_data(integration)
+            local_temp, local_humid = self._get_sensor_data(integration)
             if local_temp and local_humid:
-                comfort_levels = self.predict_comfort_level(labels, local_temp,
-                                                            local_humid,
-                                                            integration)
+                comfort_levels = self._predict_comfort_level(labels,
+                                                             local_temp,
+                                                             local_humid,
+                                                             integration)
                 response_data['comfort_level'] = comfort_levels
 
-            self.delete_excess_images('uploads')
-            self.delete_excess_images('detected_images')
+            self._delete_excess_images('uploads')
+            self._delete_excess_images('detected_images')
 
             return Response(response_data, status=status.HTTP_201_CREATED)
         else:
             return Response({'error': 'Missing required data'},
                             status=status.HTTP_400_BAD_REQUEST)
 
-    def save_image(self, image_file):
+    def _get_or_create_integration(self, secret: str) -> Integration:
+        """
+        Get or create an Integration object based on the secret.
+
+        :param secret: The secret key for the integration.
+        :type secret: str
+        :return: The Integration object.
+        :rtype: app.models.Integration
+        """
+        integration = Integration.objects.filter(secret=secret).first()
+        if not integration:
+            integration = Integration.objects.create(secret=secret)
+        return integration
+
+    def _save_image(self, image_file: ContentFile) -> str:
+        """
+        Save the uploaded image file.
+
+        :param image_file: The uploaded image file.
+        :type image_file: django.core.files.uploadedfile.InMemoryUploadedFile
+        :return: The path of the saved image.
+        :rtype: str
+        """
         image_path = default_storage.save('uploads/' + image_file.name,
                                           ContentFile(image_file.read()))
         return "media/" + image_path
 
-    def save_predictions(self, labels, integration):
+    def _segment_image(self, image_path: str) -> tuple:
+        """
+        Segment the image using the ImageSegmentation model.
+
+        :param image_path: The path of the image to segment.
+        :type image_path: str
+        :return: The annotated image and labels.
+        :rtype: tuple(numpy.ndarray, list)
+        """
+        segmentation = ImageSegmentation()
+        annotated_image, labels = segmentation.segment_image(image_path)
+        return annotated_image, labels
+
+    def _save_predictions(self, labels: list,
+                          integration: Integration) -> None:
+        """
+        Save the predicted labels.
+
+        :param labels: The predicted labels.
+        :type labels: list
+        :param integration: The Integration object.
+        :type integration: app.models.Integration
+        """
         for upper, lower in labels:
             prediction_serializer = PredictionSerializer(
                 data={'predicted_upper': upper, 'predicted_lower': lower})
             if prediction_serializer.is_valid():
                 prediction_serializer.save(integration=integration)
 
-    def save_annotated_image(self, annotated_image, request, integration):
+    def _save_annotated_image(self, annotated_image: np.ndarray,
+                              request: Request,
+                              integration: Integration) -> None:
+        """
+        Save the annotated image.
+
+        :param annotated_image: The annotated image.
+        :type annotated_image: numpy.ndarray
+        :param request: The HTTP request.
+        :type request: rest_framework.request.Request
+        :param integration: The Integration object.
+        :type integration: app.models.Integration
+        """
         _, frame = cv2.imencode('.png', annotated_image)
         img_file = ContentFile(frame.tobytes())
         img_file.name = 'segmented_image.jpg'
@@ -78,7 +135,13 @@ class PredictViewSet(viewsets.ViewSet):
         else:
             raise Exception(image_serializer.errors)
 
-    def delete_excess_images(self, folder):
+    def _delete_excess_images(self, folder: str) -> None:
+        """
+        Delete excess images from the specified folder.
+
+        :param folder: The folder name.
+        :type folder: str
+        """
         media_folder = os.path.join(settings.MEDIA_ROOT, folder)
         images = [img for img in os.listdir(media_folder)]
         if len(images) > 10:
@@ -87,16 +150,24 @@ class PredictViewSet(viewsets.ViewSet):
                 image_path = os.path.join(media_folder, image)
                 os.remove(image_path)
 
-    def create_sensor_data(self, local_temp, local_humid, integration):
-        sensor_serializer = SensorSerializer(
-            data={'local_temp': local_temp, 'local_humid': local_humid})
-        if sensor_serializer.is_valid():
-            sensor_serializer.save(integration=integration)
-        else:
-            raise Exception(sensor_serializer.errors)
+    def _predict_comfort_level(self, labels: list,
+                               local_temp: float,
+                               local_humid: float,
+                               integration: Integration) -> list:
+        """
+        Predict the comfort level based on labels, temperature, and humidity.
 
-    def predict_comfort_level(self, labels, local_temp, local_humid,
-                              integration):
+        :param labels: The predicted labels.
+        :type labels: list
+        :param local_temp: The local temperature.
+        :type local_temp: float
+        :param local_humid: The local humidity.
+        :type local_humid: float
+        :param integration: The Integration object.
+        :type integration: app.models.Integration
+        :return: The predicted comfort levels.
+        :rtype: list
+        """
         comfort_classifier = ComfortClassifier()
         comfort_levels = comfort_classifier.predict_comfort_level(labels,
                                                                   local_temp,
@@ -111,7 +182,18 @@ class PredictViewSet(viewsets.ViewSet):
                 raise Exception(comfort_serializer.errors)
         return comfort_data
 
-    def get_response_data(self, integration, request):
+    def _get_response_data(self, integration: Integration,
+                           request: Request) -> dict:
+        """
+        Get the response data for the given integration.
+
+        :param integration: The Integration object.
+        :type integration: app.models.Integration
+        :param request: The HTTP request.
+        :type request: rest_framework.request.Request
+        :return: The response data.
+        :rtype: dict
+        """
         predictions = Prediction.objects.filter(integration=integration)
         images = Image.objects.filter(integration=integration)
         sensors = Sensor.objects.filter(integration=integration)
@@ -124,7 +206,15 @@ class PredictViewSet(viewsets.ViewSet):
         }
         return response_data
 
-    def get_sensor_data(self, integration: Integration):
+    def _get_sensor_data(self, integration: Integration) -> tuple:
+        """
+        Get the sensor data for the given integration.
+
+        :param integration: The Integration object.
+        :type integration: app.models.Integration
+        :return: The local temperature and humidity.
+        :rtype: tuple(float, float)
+        """
         sensor_data = Sensor.objects.filter(integration=integration).first()
         if not sensor_data:
             return None, None
